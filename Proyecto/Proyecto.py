f7d887166375f4b60364bbedde615b71d4d53d56
EduardoMPB17 <lalitomariqueo@gmail.com>
2022-09-08 15:36:56 -0300

    clas

--- Proyecto/Proyecto.py
+++ Proyecto/Proyecto.py
@@ -1,2 +1,9 @@
 v=("hola mundo")
-def __init__(self, name=None, description=None,)
+def __init__(self, Rut, Nombre, ApellidoPaterno, ApellidoMaterno, Edad, CorreoElectronico):
+        # Parámetros
+        self.Rut = Rut # parametros
+        self.Nombre = Nombre#
+        self.ApellidoPaterno = ApellidoPaterno
+        self.ApellidoMaterno = ApellidoMaterno
+        self.Edad = Edad
+        self.CorreoElectronico = CorreoElectronico
